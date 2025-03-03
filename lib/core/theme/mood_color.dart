86d81be3673ec9e553b925d9ee2ac596c8fa4fcb
조호준 <hojun@johojun-ui-MacBookAir.local>
2025-03-03 22:18:22 +0900

    feat: MoodColor 추가
    firebase storage 제거

---
+++ lib/core/theme/mood_color.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+abstract class MoodColor {
+  MoodColor._();
+
+  // app background
+  static const appRed = Color(0xffFBD5BD);
+  static const appWhite = Color(0xffFAEEEE);
+  static const appGreen = Color(0xffDEE0D1);
+  static const appBlue = Color(0xffDBE2EE);
+  static const appPurple = Color(0xffE3DBEE);
+
+  // widget background
+  static const white = Color(0xffF5F6F5);
+
+  // text
+  static const black = Color(0xff2F3130);
+}
