9144960b6467ea76f0185bcea8068e592cc52f06
Moon <1tjsen@naver.com>
2025-05-01 16:06:18 +0900

    refactor: device preivew 제거

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,3 @@
-import 'package:device_preview/device_preview.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -22,11 +21,9 @@ class MoodTrackerApp extends ConsumerWidget {
           routerConfig: ref.watch(routerProvider),
           title: "App Title".tr(),
           theme: appThemeData(data, context.locale),
-          builder: DevicePreview.appBuilder,
           localizationsDelegates: context.localizationDelegates,
           supportedLocales: context.supportedLocales,
-          // locale: context.locale,
-          locale: DevicePreview.locale(context),
+          locale: context.locale,
         );
       },
       error: (error, stackTrace) {
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:device_preview/device_preview.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -10,19 +9,16 @@ void main() async {
   await Isar.initializeIsarCore();
   await EasyLocalization.ensureInitialized();
   runApp(
-    DevicePreview(
-      enabled: false,
-      builder: (context) => EasyLocalization(
-        supportedLocales: const [
-          Locale('en', 'US'),
-          Locale('ko', 'KR'),
-          Locale('ja', 'JP'),
-        ],
-        path: 'assets/translations',
-        fallbackLocale: const Locale('en', 'US'),
-        child: const ProviderScope(
-          child: MoodTrackerApp(),
-        ),
+    EasyLocalization(
+      supportedLocales: const [
+        Locale('en', 'US'),
+        Locale('ko', 'KR'),
+        Locale('ja', 'JP'),
+      ],
+      path: 'assets/translations',
+      fallbackLocale: const Locale('en', 'US'),
+      child: const ProviderScope(
+        child: MoodTrackerApp(),
       ),
     ),
   );
