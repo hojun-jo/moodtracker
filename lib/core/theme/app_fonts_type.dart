c80fffb712855fc4eceaa856fa54859d97bf4986
Moon <1tjsen@naver.com>
2025-05-22 15:45:20 +0900

    test: 테스트 환경에서 GoogleFonts를 직접 로딩하지 않도록 수정

--- lib/core/theme/app_fonts_type.dart
+++ lib/core/theme/app_fonts_type.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 enum AppFontsType {
@@ -11,28 +12,39 @@ enum AppFontsType {
   static final _properties = {
     AppFontsType.jua: (
       text: "Jua",
-      style: GoogleFonts.jua(),
-      theme: GoogleFonts.juaTextTheme(),
+      style:
+          kDebugMode ? const TextStyle(fontFamily: 'Jua') : GoogleFonts.jua(),
+      theme: kDebugMode ? const TextTheme() : GoogleFonts.juaTextTheme(),
     ),
     AppFontsType.hiMelody: (
       text: "Hi Melody",
-      style: GoogleFonts.hiMelody(),
-      theme: GoogleFonts.hiMelodyTextTheme(),
+      style: kDebugMode
+          ? const TextStyle(fontFamily: 'Hi Melody')
+          : GoogleFonts.hiMelody(),
+      theme: kDebugMode ? const TextTheme() : GoogleFonts.hiMelodyTextTheme(),
     ),
     AppFontsType.cute: (
       text: "Cute",
-      style: GoogleFonts.cuteFont(),
-      theme: GoogleFonts.cuteFontTextTheme(),
+      style: kDebugMode
+          ? const TextStyle(fontFamily: 'Cute')
+          : GoogleFonts.cuteFont(),
+      theme: kDebugMode ? const TextTheme() : GoogleFonts.cuteFontTextTheme(),
     ),
     AppFontsType.nanumGothic: (
       text: "Nanum Gothic",
-      style: GoogleFonts.nanumGothic(),
-      theme: GoogleFonts.nanumGothicTextTheme(),
+      style: kDebugMode
+          ? const TextStyle(fontFamily: 'Nanum Gothic')
+          : GoogleFonts.nanumGothic(),
+      theme:
+          kDebugMode ? const TextTheme() : GoogleFonts.nanumGothicTextTheme(),
     ),
     AppFontsType.nanumMyeongjo: (
       text: "Nanum Myeongjo",
-      style: GoogleFonts.nanumMyeongjo(),
-      theme: GoogleFonts.nanumMyeongjoTextTheme(),
+      style: kDebugMode
+          ? const TextStyle(fontFamily: 'Nanum Myeongjo')
+          : GoogleFonts.nanumMyeongjo(),
+      theme:
+          kDebugMode ? const TextTheme() : GoogleFonts.nanumMyeongjoTextTheme(),
     ),
   };
 
