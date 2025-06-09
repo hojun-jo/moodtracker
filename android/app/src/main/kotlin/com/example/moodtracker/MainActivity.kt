1f8f54d55bf0511b2883048e3803093ff24d408e
Moon <1tjsen@naver.com>
2025-06-09 16:12:34 +0900

    fix: 안드로이드 패키지 이름 수정

--- android/app/src/main/kotlin/com/example/moodtracker/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.moodtracker
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ android/app/src/main/kotlin/moon/asis/MainActivity.kt
@@ -0,0 +1,5 @@
+package moon.asis
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() 
