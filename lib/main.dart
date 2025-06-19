f52646850901f4d729164a4412cc6415be0f29df
Moon <1tjsen@naver.com>
2025-06-19 13:56:43 +0900

    fix: 안드로이드 버튼 네비게이션에 화면이 가리는 문제 수정

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:isar/isar.dart';
 import 'package:moodtracker/app.dart';
 
+// TODO: 구글 폰트 뜯어보기, 스캐터차트 화면 아래 글자 잘림(다이나믹타입?)
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Isar.initializeIsarCore();
