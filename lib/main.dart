43373f54f7901fb5760322d20238f6d0f19100e1
Moon <1tjsen@naver.com>
2025-07-22 01:24:04 +0900

    feat: flutter_dotenv 패키지 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:isar/isar.dart';
 import 'package:moodtracker/app.dart';
@@ -8,6 +9,7 @@ import 'package:moodtracker/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
